45b179d5f8d797ffc0a7dab7cbf97c604b092625
CesarOH <cesar.hernandez1094@gmail.com>
2014-11-03 10:56:44 -0500

    login.php

---
+++ users/login.php
@@ -0,0 +1,35 @@
+<?php
+
+include('../assests/application_header.php')
+
+?>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Login Form</title>
+</head>
+<body>
+  <section class="container">
+    <div class="login">
+      <h1>Login</h1>
+      <form method="post" action="index.php">
+        <p><input type="text" name="login" value="" placeholder="Username or Email"></p>
+        <p><input type="password" name="password" value="" placeholder="Password"></p>
+        <p class="remember_me">
+          <label>
+            <input type="checkbox" name="remember_me" id="remember_me">
+            Remember me on this computer
+          </label>
+        </p>
+        <p class="submit"><input type="submit" name="commit" value="Login"></p>
+      </form>
+    </div>
+
+    <div class="login-help">
+      <p>Forgot your password? <a href="index.html">Click here to reset it</a>.</p>
+    </div>
+  </section>
+</body>
+</html>
