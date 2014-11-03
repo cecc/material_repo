c3bb5288c2231a0bca1bee9fe333bee47669134b
CesarOH <cesar.hernandez1094@gmail.com>
2014-11-03 12:24:21 -0500

    Update retrieve.php

--- users/retrieve.php
+++ users/retrieve.php
@@ -3,7 +3,7 @@
 include('../assets/application_header.php')
 include('../assets/connect.php');
 
-$result = mysql_query("SELECT * FROM repo_user");
+$result = mysql_query("SELECT * FROM repo_organization");
 
 if ($result){
 		
