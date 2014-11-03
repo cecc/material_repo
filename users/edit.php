65ac49a7127888a88619b7e93f0ea05c06adc7fb
al285 <al285@njit.edu>
2014-11-03 11:42:24 -0500

    Create edit.php

---
+++ users/edit.php
@@ -0,0 +1,33 @@
+<?php
+
+include('../assets/application_header.php');
+
+
+?>
+
+<!DOCTYPE html>
+
+
+<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+
+name: <input type="text" name="name"><br>
+email: <input type="text" name="email"><br>
+phoneNumber:<input type="text" name="phoneNumber"><br>
+secondaryPhone: <input type="text" name="secondaryPhone"><br>
+type:<input type="text" name="type"><br>
+status: <input type="boolean" name="status"><br>
+password: <input type="text" name="password"><br>
+organization_id: <input type="text" name="organization_id"><br>
+
+
+
+
+
+
+</form>
+
+
+
+
+
+</html>
