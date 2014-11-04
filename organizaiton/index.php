fa3dd3ac3400015f84590f13320ea0c6b6f05c70
authorpenzwell <ko45@njit.edu>
2014-11-04 11:27:57 -0500

    Update index.php

---
+++ organizaiton/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<body>
+
+	<h1>Welcome Focus</h1>
+
+		<h2>On this page, you will be able to edit and update your organization's content and files.</h2>
+
+
+	<p>In 2012, 49.0 million Americans lived in a food-insecure household of that there were 33.1 million were adults and 15.9 million were children. In the same year, 59.4 percent of food insecure households particiapted in at least one of the three major Federal food assistance programs: Supplementals Nutrition Assistance Program (formerly Food Stamp Program), The National School Lunch Program, and the Special Supplemental Nutrition Program for Women, Infants and Children.</p>
+
+	<h2>Click <a href="http://web.njit.edu/">here</a> to edit your page!</h2>
+
+
+	</body>
+</html>
